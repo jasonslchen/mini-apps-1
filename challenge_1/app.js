ae22684cc6a8734516db0034733c82765a23ccab
Jason Chen <jason.sl.chen@gmail.com>
2019-08-12 11:33:54 -0700

    get board reset to work

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,8 @@
+let board = ['1A', '1B', '1C', '2A', '2B', '2C', '3A', '3B', '3C'];
 
-
+//Reset Board
+document.getElementById("reset").onclick = function() {
+  board.forEach((position) => {
+    document.getElementById(position).innerHTML = '';
+  })
+}
