2a29bc0ae6d85824d26b8b03e01f71b9f8d793c9
Jason Chen <jason.sl.chen@gmail.com>
2019-08-13 15:31:32 -0700

    parse data into proper csv format and can write file to file system

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -16,17 +16,39 @@ app.post('/', (req, res) => {
   let resultsArr = [];
   let bodyContent = req.body.content;
   let parse = JSON.parse(bodyContent.slice(0, bodyContent.length-1));
-  let firstRow = Object.keys(parse);
-  resultsArr.push(firstRow.join(','));
-
+  
   function makeCSV(jsonObj) {
+    let personArr = [];
     for (let key in jsonObj) {
-      
+      if (key !== "children") {
+        personArr.push(jsonObj[key]);
+      } else if (key === 'children') {
+        resultsArr.push(personArr.join(','));
+        if (jsonObj[key].length !== 0) {
+          jsonObj[key].forEach((child) => {
+            makeCSV(child);
+          })
+        }
+      }
     }
   }
+  
+  makeCSV(parse);
+  
+  let firstRow = Object.keys(parse);
+  firstRow = firstRow.slice(0, firstRow.length - 1);
+  resultsArr.unshift(firstRow.join(','));
+
+  let csvData = resultsArr.join('\n');
+  
+  fs.writeFile('file.csv', csvData, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  })
 
 
-  res.status(200).send(firstRow);
+  res.status(200).send(csvData);
 })
 
 
