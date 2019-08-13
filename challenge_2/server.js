03e955dc57f954dd408ac8ec2bd41658c436be42
Jason Chen <jason.sl.chen@gmail.com>
2019-08-13 12:29:43 -0700

    express server accepts a post request from html client

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(express.static('client'));
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.status(200).send(req.body);
+})
+
+
+
+app.listen(port, () => console.log(`Server be listening on port ${port}`));
 
